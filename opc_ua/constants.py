bc705547129e3899ab2611b5b1b79542846d9347
arondy <antongatcenko@gmail.com>
2025-10-14 15:54:22 +0300

    Added OPC UA Client-Server model

---
+++ opc_ua/constants.py
@@ -0,0 +1,5 @@
+OPC_UA_URL_SERVER = "opc.tcp://localhost:4840/freeopcua/server/"
+OPC_UA_URL_CLIENT = "opc.tcp://admin@localhost:4840/freeopcua/server/"
+OPC_UA_NAMESPACE = "opcua_ns"
+OPC_UA_VARIABLE_NAME = "opcua_var"
+OPC_UA_VARIABLE_ID = 1
