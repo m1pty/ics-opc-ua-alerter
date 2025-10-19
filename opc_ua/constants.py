d8f5fb799e9728ea5cfa48f87c493cd43d6f0fa0
arondy <antongatcenko@gmail.com>
2025-10-19 22:53:49 +0300

    change name to opcua

--- opc_ua/constants.py
+++
@@ -1,5 +0,0 @@
-OPC_UA_URL_SERVER = "opc.tcp://localhost:4840/freeopcua/server/"
-OPC_UA_URL_CLIENT = "opc.tcp://admin@localhost:4840/freeopcua/server/"
-OPC_UA_NAMESPACE = "opcua_ns"
-OPC_UA_VARIABLE_NAME = "opcua_var"
-OPC_UA_VARIABLE_ID = 1
